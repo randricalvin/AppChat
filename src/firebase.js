5bb7ca155e6611cc07a12b3b3f3906551258f798
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-10-14 17:22:42 +0200

    config register

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBf2bWWqIlJd7pMlS1kppyQBVMAE9P-ONo",
@@ -8,9 +10,11 @@ const firebaseConfig = {
   storageBucket: "appchat-dfa39.appspot.com",
   messagingSenderId: "197125358384",
   appId: "1:197125358384:web:f6cff5b4cd098fcb10bef5",
-  measurementId: "G-B25EK4LQY1"
+  measurementId: "G-B25EK4LQY1",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,37 +1,64 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import add from '../assets/add.svg'
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from '../firebase'
+import {ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore"; 
+
 
 
 const Register = () => {
+  const [error, setError] = useState(false)
+  // const navigate = useNavigate ()
 
-  // const [firstName, setFirstName] = useState('')
-  // const [lastName, setlastName] = useState('')
-  // const [email, setEmail] = useState('')
-  // const [password, setPassword] = useState('')
-
-  const handleSubmit = (e) => {
+  // User registration
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    const firstName = e.target[0].value
-    const lastName = e.target[1].value
-    const email = e.target[2].value
-    const password = e.target[3].value
-    const file = e.target[4].files[0]
+    const displayName = e.target[0].value
+    const email = e.target[1].value
+    const password = e.target[2].value
+    const file = e.target[3].files[0]
 
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in 
-    const user = userCredential.user;
-    console.log(user)
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
-  }
+
+    try {
+
+    const res = createUserWithEmailAndPassword(auth, email, password)
+
+    // Upload image user to firebase storage for registration
+    const storageRef = ref(storage, displayName)
+
+    const uploadTask = uploadBytesResumable(storageRef, file);
+  
+    uploadTask.on('state_changed',
+      (error) => {
+        setError(true)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async(downloadURL) => {
+          await updateProfile(res.user, {
+            displayName,
+            photoURL: downloadURL,
+          });
+          // Add a user in database (firestore)
+          await setDoc(doc(db, "users", res.user.uid), {
+            uid: res.user.uid,
+            displayName,
+            email,
+            photoURL: downloadURL,
+          });
+          // Add a userchats in database (firestore)
+          // await setDoc(doc(db, "userchats", res.user.uid), {});
+          //   navigate("/dashboard")
+          
+        });
+      }
+    );
+    } catch(error) {
+      setError(true)
+    }
+}
+
 
   return (
     <div>
@@ -42,45 +69,33 @@ createUserWithEmailAndPassword(auth, email, password)
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         My AppChat
                     </h1>
-                    <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleSubmit}>
+                    <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
                       <div>
-                          <label for="text" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Firstname</label>
+                          <label htmlFor="text" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Display name</label>
                           <input 
-                          type="text" 
-                          id="text" 
+                          type="text"  
                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                          placeholder="John" 
+                          placeholder="display name" 
                           />
                       </div>
                       <div>
-                          <label for="text" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Lastname</label>
-                          <input 
-                          type="text" 
-                          id="text" 
-                          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                          placeholder="Doe" 
-                          />
-                      </div>
-                      <div>
-                          <label for="email" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                          <label htmlFor="email" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                           <input 
                           type="email" 
-                          id="email" 
                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                           placeholder="name@company.com" 
                           />
                       </div>
                       <div>
-                          <label for="password" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                          <label htmlFor="password" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                           <input 
                           type="password" 
-                          id="password" 
-                          placeholder="••••••••" 
+                          placeholder="password" 
                           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                           />
                       </div>
                       <div className="flex items-center">
-                          <label for="file" className="flex cursor-pointer text-sky-600 text-sm font-medium dark:text-white">
+                          <label htmlFor="file" className="flex cursor-pointer text-sky-600 text-sm font-medium dark:text-white">
                             <img  src={add} alt="add avatar"/>
                             <span className="mx-2">
                               Add your avatar
@@ -98,10 +113,11 @@ createUserWithEmailAndPassword(auth, email, password)
                       className="w-full text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
                         Create an account
                       </button>
-                      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                          Already have an account? <Link to='/login' className="text-sky-600 font-medium text-primary-600 hover:text-sky-700 hover:underline dark:text-primary-500">Login here</Link>
-                      </p>
+                      {error && <p className="text-red-500 text-xs italic">Something went wrong</p>}
                     </form>
+                    <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                        Already have an account? <Link to='/login' className="text-sky-600 font-medium text-primary-600 hover:text-sky-700 hover:underline dark:text-primary-500">Login here</Link>
+                    </p>
                 </div>
             </div>
         </div>
