addf28bc9e910e6d5c77f4daba14475e7030e16f
Aimé Calvin RANDRIANANTENAINA <randricalvin@gmail.com>
2022-10-27 12:49:58 +0200

    chat data structure

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import { collection, query, where, getDocs, setDoc, doc } from "firebase/firestore";
+import React, { useState, useContext } from 'react'
+import { collection, query, where, getDocs, getDoc, setDoc, doc, updateDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebase'
-import { AuthContext } from '../contexts/AuthContext'
+import { AuthContext } from '../context/AuthContext'
 
 const SearchBar = () => {
   const [userName, setUserName] = useState('')
@@ -32,53 +32,71 @@ const SearchBar = () => {
   // Select a user search result
   const handleSelect = async() => {
     //  Check the group chat in firestore exists, if not create it
-    const combinedId = currentUser.uid > user.id ? currentUser.uid + user.id : user.id + currentUser.uid;
+    const combinedId = currentUser.uid > user.id 
+    ? currentUser.uid + user.id 
+    : user.id + currentUser.uid;
     
     try {
-      const res = await getDocs (db, "chats", combinedId)
+      const res = await getDoc(doc(db, "chats", combinedId));
 
-      if (!res.exists) {
+      if (!res.exists()) {
         // Create a chat in chats collection
-        await setDoc(doc,(db, "chats", combinedId), {message: []})
+        await setDoc(doc(db, "chats", combinedId), {messages: []})
 
         // Create a user chats
+        await updateDoc(doc(db, "userchats", currentUser.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: user.uid,
+            displayName: user.displayName,
+            photoURL: user.photoURL
+        },
+        [combinedId + ".date"]: serverTimestamp()
+        });
+
+        await updateDoc (doc(db, "userchats", user.uid), {
+          [combinedId + ".userInfo"]: {
+            uid: currentUser.uid,
+            displayName: currentUser.displayName,
+            photoURL: currentUser.photoURL
+        },
+        [combinedId + ".date"]: serverTimestamp()
+        })
       }
-    }
+    } 
     catch(err) {
       setErr(true)
       console.log(err)
     }
-
-    //  Create user chats
-
+    
+    setUser (null)
+    setUserName("")
+  }
+    
+    
 
   return (
     <div className='flex flex-col justify-center'>
       <div className='flex justify-center'>
           <input
-          className="bg-lightGrey my-4 h-10 lg:w-5/6 md:w-4/6 sm:w-2/3 pl-5 text-m focus:outline-none rounded-lg"
+          className="bg-lightGrey my-4 h-10 lg:w-full md:w-full sm:w-2/3 pl-5 text-m focus:outline-none rounded-lg"
           name="search"
           type="search"
           placeholder="Find an user"
           onKeyDown={handleKey}
           onChange={(e) => setUserName(e.target.value)}
+          value={userName}
           />
       </div>
       {err && <p className='text-red-500 text-center'>User not found</p>}
-      { user && <div className="flex py-4 px-2 justify-start items-center hover:bg-sky-600 border-b-2">
-        <div>
+      { user && <div className="flex py-4 px-2 justify-start items-center hover:bg-sky-600 border-b-2 cursor-pointer" onClick={handleSelect}>
           <img
             src={user.photoURL}
             className="object-cover h-10 w-10 rounded-full"
-            alt=""
+            alt="user avatar"
           />
-        </div>
-        <div className="mx-4">
-          <div className="text-m font-semibold text-white">
+          <div className="mx-4 text-m font-semibold text-white">
             {user.displayName}
           </div>
-          <p className="text-white">Pick me at 9:00 Am</p>
-        </div>
       </div>}
     </div>
   )
